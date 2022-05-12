f4e65e934a268df2ff00b8e11caadef5defed44e
pomazan19 <poma205@gmail.com>
2022-05-12 13:34:04 -0300

    Agregar archivos de proyecto.

---
+++ ProductoApp/Suelto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductoApp
+{
+    internal class Suelto : Producto
+    {
+        private float medida;
+
+        public Suelto(): base() 
+        {
+            medida = 0;
+        }
+
+        public Suelto(int codigo, string marca, float precioUnitario, float medida) : base(codigo, marca, precioUnitario)
+        {
+            this.medida = medida;
+        }
+       
+        public float Medida
+        {
+            get { return medida; }
+            set { medida = value; }
+        }
+        //sobreescribiendo un comportamieento heredado para especializar
+        public override string ToString()
+        {
+            return base.ToString() + " |Medida: " + medida;
+        }
+
+
+        public override 0.. GetPrecio()
+        {
+            return medida * PrecioUnitario;
+        }
+
+
+    }
+}
